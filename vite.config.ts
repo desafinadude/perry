c2f5dab62a5e03473484e440d6155aed792e6aed
desafinadude <meerkdir@gmail.com>
2026-07-01 10:34:19 +0200

    chore: dev server port 1234, update perry startup script with MIDI reset

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 1234,
+  },
   preview: {
-    port: 1111,
-    cors: true
+    port: 1234,
+    cors: true,
   },
 })
